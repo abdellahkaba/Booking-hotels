77fe87439ecba832dcfa80e14fc93bb64a633c13
Abdellah <abdallahkaba98@gmail.com>
2024-10-19 17:34:31 +0000

    Install toastr notification

--- bookingRoom-ui/src/app/app.config.ts
+++ bookingRoom-ui/src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideToastr} from "ngx-toastr";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideToastr({
+      progressBar: true,
+      closeButton: true,
+      newestOnTop: true,
+      tapToDismiss: true,
+      positionClass: 'toast-top-right',
+      timeOut: 8000
+    })
+  ]
 };
